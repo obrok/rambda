e3c1d3858f78b40a0c51d10a414fc747e52e2b8a
Paweł Obrok <pawel.obrok@gmail.com>
2018-03-01 23:43:21 +0100

    Read program from stdin and evaluate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 extern crate rambda;
 
-fn main() {
-    let mut expression = String::new();
-    std::io::stdin()
-        .read_line(&mut expression)
-        .expect("should work");
-    let ast = rambda::parse(&expression);
+use std::io::Read;
 
-    println!("{:?}", &ast);
+fn main() {
+    let mut buffer = String::new();
+    std::io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("Couldn't read STDIN");
+
+    match rambda::parse_statements(&buffer) {
+        Ok(program) => {
+            let results = rambda::eval_statements(&program);
+
+            for (statement, result) in program.iter().zip(results) {
+                println!(
+                    "{}\n-> {}\n",
+                    rambda::unparse_statement(&statement),
+                    rambda::unparse(&result)
+                );
+            }
+        }
+        Err(why) => println!("{:?}", why),
+    }
 }
